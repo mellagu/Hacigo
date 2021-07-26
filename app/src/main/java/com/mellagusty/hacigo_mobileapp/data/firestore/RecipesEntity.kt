2b5067624e0c883ba6017538de32aea2b1d918fa
Mella Aulia Agusty <agustymella@gmail.com>
2021-07-26 14:19:01 +0700

    Progress on Recipes

--- app/src/main/java/com/mellagusty/hacigo_mobileapp/data/firestore/RecipesEntity.kt
+++ app/src/main/java/com/mellagusty/hacigo_mobileapp/data/firestore/RecipesEntity.kt
@@ -3,9 +3,9 @@ package com.mellagusty.hacigo_mobileapp.data.firestore
 data class RecipesEntity(
     val judul: String? = null,
     val subJudul: String? = null,
-    val bahan: String?  = null,
-    val caraBuat : String? = null,
-    val imageUrl : String? = null
+    val bahan: ArrayList<String>,
+    val caraBuat: ArrayList<String>,
+    val imageUrl: String? = null
 
 
 )
